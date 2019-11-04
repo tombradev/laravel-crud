8e1380a628425802174467c7ebd552aed0dd48d5
tombradev <tombradev@gmail.com>
2019-11-04 15:08:16 +0700

    adding modal and forms

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -8,36 +8,83 @@
         {{-- <h1>Halo Laravel World Indonesia</h1> --}}
     <div class="container">
         <div class="row">
-                <h1>Halo Laravel World Indonesia</h1>
-                <table class="table">
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Sex</th>
-                            <th>Religion</th>
-                            <th>Address</th>
-                        </tr>
-                        @foreach ($data_siswa as $siswa)
-                        <tr>
-                            <td>{{$siswa->firstname}}</td>
-                            <td>{{$siswa->lastname}}</td>
-                            <td>{{$siswa->sex}}</td>
-                            <td>{{$siswa->religion}}</td>
-                            <td>{{$siswa->address}}</td>
-                        </tr>  
-                        @endforeach
-                    </table>
+            <div class="col-6">
+                    <h1>Halo Laravel World Indonesia</h1>
+            </div>
+            <div class="col-6">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary btn-sm float-md-right" data-toggle="modal" data-target="#exampleModal">
+                        Tambah Siswa
+                    </button>
+            </div>
+            <table class="table table-hover">
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Sex</th>
+                        <th>Religion</th>
+                        <th>Address</th>
+                    </tr>
+                    @foreach ($data_siswa as $siswa)
+                    <tr>
+                        <td>{{$siswa->firstname}}</td>
+                        <td>{{$siswa->lastname}}</td>
+                        <td>{{$siswa->sex}}</td>
+                        <td>{{$siswa->religion}}</td>
+                        <td>{{$siswa->address}}</td>
+                    </tr>  
+                    @endforeach
+                </table>
         </div>
     </div>
-        <h1>Halo Laravel World Indonesia</h1>
-        {{-- {{dd($data_siswa)}} {{-- -- check if the data is going to the view from DB --}} 
+                
+{{--         <h1>Halo Laravel World Indonesia</h1>
+{{-- {{dd($data_siswa)}} {{-- -- check if the data is going to the view from DB --}}  --}}
         
-        
+    {{--  Bootstrap component  --}}
 
-       {{--  bootstrap --}}
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+      
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                    <form>
+                            <div class="form-group">
+                              <label for="exampleInputEmail1">Email address</label>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div class="form-group">
+                              <label for="exampleInputPassword1">Password</label>
+                              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <div class="form-group form-check">
+                              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    {{--  bootstrap --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
 </body>
 </html>
 
